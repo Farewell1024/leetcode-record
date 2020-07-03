fa65f4c56987bec7f3bd62b6039bbf417ee15abf
zhangyu <zhangyu@xingheng.ai>
2020-07-03 10:51:58 +0800

    init

---
+++ src/main/java/com/zhangyu/leetcoderecord/LeetcodeRecordApplication.java
@@ -0,0 +1,13 @@
+package com.zhangyu.leetcoderecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeRecordApplication.class, args);
+	}
+
+}
