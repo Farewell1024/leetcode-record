67f0a1e323483dd73512b3cb73e71132e0e87720
zhangyu <zhangyu@xingheng.ai>
2021-03-15 17:55:28 +0800

    fix bug

--- src/main/java/com/zhangyu/leetcoderecord/bytedance/linkedListAndTree/Solution0160.java
+++ src/main/java/com/zhangyu/leetcoderecord/bytedance/linkedListAndTree/Solution0160.java
@@ -33,6 +33,16 @@ public class Solution0160 {
         n5.next = n6;
         n6.next = n2;
         System.out.println(solution.getIntersectionNode(h1, h2));
+
+        ListNode l1 = new ListNode(2);
+        ListNode l2 = new ListNode(6);
+        ListNode l3 = new ListNode(4);
+        ListNode l4 = new ListNode(1);
+        ListNode l5 = new ListNode(5);
+        l1.next = l2;
+        l2.next = l3;
+        l4.next = l5;
+        System.out.println(solution.getIntersectionNode(l1, l4));
     }
 
     /**
@@ -56,7 +66,7 @@ public class Solution0160 {
     /**
      * 双指针
      * 每个链表需要遍历两次
-     *
+     * <p>
      * 从两个节点的头开始遍历，假设当A节点到末尾的时候，将A节点指向B节点的开头
      * 继续遍历，当B节点到末尾的时候，将B节点指向A节点的开头。
      * 这样A节点到末尾的距离 就等于B节点到末尾的距离了
@@ -64,10 +74,11 @@ public class Solution0160 {
      * 如果无相交节点的话，两哥指针会同时到达链表末尾
      */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
         ListNode n1 = headA, n2 = headB;
         while (n1 != n2) {
-            n1 = (n1.next == null ? headB : n1.next);
-            n2 = (n2.next == null ? headA : n2.next);
+            n1 = (n1 == null ? headB : n1.next);
+            n2 = (n2 == null ? headA : n2.next);
         }
         return n1;
     }
